aae13d6d534088180f1c3e85fcb2dcf0dbe5d186
EsraaAlzoghby <esraa.m.zogpy@gmail.com>
2025-09-03 18:30:24 +0300

    oop2 commit

---
+++ CSharp_OOP/Task 1/Seas on/Seas on/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Seas_on
+{
+    public enum Season
+    {
+        spring, summer, autumn, winter
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a season:");
+
+            foreach (Season item  in Enum.GetValues(typeof(Season)))
+            {
+                Console.WriteLine(item);
+            }
+
+          if (Enum.TryParse<Season>(Console.ReadLine() , out Season seasontest))
+            {
+
+                if (seasontest == Season.spring)
+                {
+                    Console.WriteLine("Spring: March to May");
+                }
+                else if (seasontest == Season.winter)
+                {
+                    Console.WriteLine("Winter: December to February");
+                }
+                else if (seasontest == Season.summer)
+                {
+                    Console.WriteLine("Summer: June to August");
+                }
+                else if (seasontest == Season.autumn)
+                {
+                    Console.WriteLine("Autumn: September to November");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid season name!");
+            }
+
+        }
+    }
+}
--- CSharp_OOP/Task 1/WeekDays/WeekDays/Program.cs
+++ CSharp_OOP/Task 1/WeekDays/WeekDays/Program.cs
@@ -1,14 +1,33 @@
-﻿/*
- 1-Create an Enum called "WeekDays" with the days of the week (Monday to Sunday) as its members. Then,
-write a C# program that prints out all the days of the week using this Enum.
- */
+﻿ //Create an Enum called "WeekDays" with the days of the week (Monday to Sunday) as its members. 
+ //Then, write a C# program that prints out all the days of the week using this Enum.
 namespace WeekDays
 {
+    public enum WeekDays
+    {
+
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+
+    }
+   //Console.WriteLine(typeof(WeekDays));
+   // typeof(WeekDays): returns the type information of the Enum (WeekDays) itself
+   // Enum.GetValues(typeof(WeekDays)): returns all values inside the Enum as an array
+
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Days of the Week:");
+            foreach (WeekDays item in Enum.GetValues(typeof(WeekDays)))
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
