756734a65b2040d9c5b55cc95bcc85e10eb0a53b
EsraaAlzoghby <esraa.m.zogpy@gmail.com>
2025-09-11 21:36:38 +0300

    task3oop commit

---
+++ CSharp_OOP/Task 3/LibraryManagementSystem/LibraryManagementSystem/EBook .cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem
+{    // Derived class for EBooks
+
+    internal class EBook:Book
+    {
+        public double FileSize { get; set; }
+        public EBook(string title, string author, string isbn , double filesize):base(title , author , isbn)
+        {
+             FileSize = filesize;
+        }
+        public override void DisplayInfo()
+        {
+            base.DisplayInfo();
+            Console.WriteLine($"File Size: {FileSize} MB");
+        }
+    }
+}
---
+++ CSharp_OOP/Task 3/employees/employees/Program.cs
@@ -0,0 +1,60 @@
+﻿//Define a class Employees with fields: ID, Name, Gender, Salary, HireDate, and SecurityLevel.
+//Use an Enum Security with values: Guest = 1, Developer = 2, Secretary = 3, DBA = 4.
+//Restrict Gender to accept only M/F or male/female.
+//Override ToString() to display employee details (Salary in currency format, HireDate in short date format).
+//In Main(), ask the user for the number of employees, read their details with validation, store them in a List, and print all employees.
+namespace employees
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employees> employees = new List<Employees>();
+            Console.Write("Enter number of employees: ");
+            int count = int.Parse(Console.ReadLine());
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine($"\nEnter details for Employee #{i + 1}:");
+
+                Console.Write("ID: ");
+                int id;
+                while (!int.TryParse(Console.ReadLine(), out id))
+                {
+                    Console.Write(" Invalid input! Enter a valid number for ID: ");
+                }
+                Console.Write("Name: ");
+                string? name = Console.ReadLine();
+
+                Console.Write("Gender (male(m)/female(f)): ");
+                string? gender = Console.ReadLine();
+
+                Console.Write("Salary: ");
+                decimal salary;
+                while (!decimal.TryParse(Console.ReadLine(), out salary))
+                {
+                    Console.Write(" Invalid input! Enter a valid number for salary: ");
+                }
+
+                DateTime hireDate;
+                Console.Write("Hire Date (yyyy-mm-dd): ");
+
+                while (!DateTime.TryParse(Console.ReadLine(), out hireDate))
+                {
+                    Console.WriteLine(" Invalid date format! Please enter again (yyyy-mm-dd): ");
+                }
+
+                Console.Write("Enter Security Level Number (1 = Guest, 2 = Developer, 3 = Secretary, 4 = DBA):  ");
+                Security Securitylevel = (Security)int.Parse(Console.ReadLine());
+
+                employees.Add(new Employees(id, name, Securitylevel, salary, hireDate, gender));
+            }
+
+            Console.WriteLine("\n========== Employees List ==========");
+            foreach (Employees item in employees)
+            {
+                Console.WriteLine(item);
+
+            }
+        }
+    }
+}
